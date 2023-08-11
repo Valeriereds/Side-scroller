b152d06a8b98382ca5a0218385ceda9b8e8d97e3
Valeriereds <valeriereds@gmail.com>
2023-08-11 12:06:27 -0500

    fixed syntax

---
+++ controllers/leaderboardRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// router.post('/leaderboards', async (req, res) => {
+//   try {
+    
+//   }
+// })
+
+
+
+module.exports = router;
