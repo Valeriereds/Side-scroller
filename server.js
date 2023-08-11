c92575ce12db9666c26b03429f1603a7e22ab83d
Abe Omaits <aomaits@gmail.com>
2023-08-11 07:02:43 -0500

    installed node packages, updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
 const express = require('express');
+// Import express-session
 const session = require('express-session');
+// Import express-handlebars
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
+// Import the connection object
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Sets up the Express App
 const app = express();
+
+// Port determined for deployment to Heroku or running locally
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
+// Set up sessions
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -34,12 +42,16 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// turn on routes
 app.use(routes);
 
+// turn on connection to db and server, force false so data doesn't get dropped on every sync
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
