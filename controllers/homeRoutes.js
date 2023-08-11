12dab91189f31ca72c31c489c7fbd5da4c58950c
micavilla <micaella.villa1@gmail.com>
2023-08-11 14:24:05 -0500

    added render for starting page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const  User = require('../models/User');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    res.render('login', {
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 router.get('/start', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
