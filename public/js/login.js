354ed9f513e1bd613d8a39489090856de4fc1580
micavilla <micaella.villa1@gmail.com>
2023-08-11 19:30:20 -0500

    adjusted id names in login.handlebars and login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,23 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
+  // email switched to player name
+  // const email = document.querySelector('#email-login').value.trim();
+  const player_name = document.querySelector('#player-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
+  if (player_name && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ player_name, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      // document.location.replace('/profile');
+      document.location.replace('/start');
     } else {
       alert(response.statusText);
     }
@@ -25,19 +27,20 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
+  const player_name = document.querySelector('#name-signup').value.trim();
+  // email unneeded methinks
+  // const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
+  if (player_name && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ player_name, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/start');
     } else {
       alert(response.statusText);
     }
