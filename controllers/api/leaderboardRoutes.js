2f65dd88eadde8a470ef5d1353c761a1fce33052
Valeriereds <valeriereds@gmail.com>
2023-08-12 22:35:51 -0500

    fixed post leaderboardRoutes

--- controllers/api/leaderboardRoutes.js
+++ controllers/api/leaderboardRoutes.js
@@ -52,10 +52,7 @@ router.get('/:id', async (req, res) => {
 
     router.post('/', async (req, res) => {
       try {
-        const newScore = await Scores.create({
-          ...req.body,
-          user_id: req.session.user_id,
-        });
+        const newScore = await Scores.create(req.body);
     
         res.status(200).json(newScore);
       } catch (err) {
