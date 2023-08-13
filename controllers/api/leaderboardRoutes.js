37c8e6bd1379fc3b18856f24729fe77a61f1a1d2
micavilla <micaella.villa1@gmail.com>
2023-08-12 22:33:23 -0500

    edited post route for scores

--- controllers/api/leaderboardRoutes.js
+++ controllers/api/leaderboardRoutes.js
@@ -10,8 +10,9 @@ router.get('/', async (req, res) => {
             {
                 model: User,
                 attributes: ['player_name'],
-            },
-          ],
+            }],
+        order: [['score', 'DESC']],
+        limit: 5,
         });
         res.status(200).json(scoreData);
         // // Serialize data so the template can read it
@@ -46,6 +47,16 @@ router.get('/', async (req, res) => {
         // });
       } catch (err) {
         res.status(500).json(err);
+      }
+    });
+
+router.post('/', async (req, res) => {
+      try {
+        const newScore = await Scores.create(req.body);
+    
+        res.status(200).json(newScore);
+      } catch (err) {
+        res.status(400).json(err);
       }
     });
 
