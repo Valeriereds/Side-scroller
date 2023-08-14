da370673addc46336f57405b187d59bc08ae3c23
Abe Omaits <aomaits@gmail.com>
2023-08-14 12:44:47 -0500

    small tweak to user.js file

--- models/User.js
+++ models/User.js
@@ -8,9 +8,9 @@ const sequelize = require('../config/connection');
 class User extends Model {
     // double-check encrypted password to confirm logins
     checkPassword(loginPw) {
-      return bcrypt.compareSync(loginPw, this.password);
+        return bcrypt.compareSync(loginPw, this.password);
     }
-  }
+}
 
 // creates and describes the user model
 User.init(
